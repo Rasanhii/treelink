c6316a3978a64a8ed7a475809a4616aa9ac77da5
Funça~ <124839957+Faelfaria@users.noreply.github.com>
2024-09-25 01:07:36 +0000

    LinkTree

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Teste
-        </a>
-      </header>
+    <div className="page">
+      <h1 className="title">Venha nos conhecer</h1>
+      <div className="container">
+        <div className="photo-card">
+          <img src="https://via.placeholder.com/150" alt="Integrante 1" className="profile-photo" />
+          <p>Integrante    g  1</p>
+          <div className="button-group">
+            <div>Butão</div>
+          </div>
+        </div>
+        <div className="photo-card">
+          <img src="https://via.placeholder.com/150" alt="Integrante 2" className="profile-photo" />
+          <p>Integrante 2</p>
+          <div className="button-group">
+          <div>Butão</div>
+          </div>
+        </div>
+        <div className="photo-card">
+          <img src="https://via.placeholder.com/150" alt="Integrante 3" className="profile-photo" />
+          <p>Integrante 3</p>
+          <div>Butão</div>
+          <div className="button-group">
+            
+          </div>
+        </div>
+        <div className="photo-card">
+          <img src="https://via.placeholder.com/150" alt="Integrante 4" className="profile-photo" />
+          <p>Integrante 4</p>
+          <div>Butão</div>
+          <div className="button-group">
+            
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

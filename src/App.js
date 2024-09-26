96f8e9b47a61ae0523af5b8f2648cf6411d717e5
Rasanhii <jonataspereira788@gmail.com>
2024-09-26 01:31:49 -0300

    Definicao de rotas

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Homepage from './pages/homepage/homepage';
 
 function App() {
   return (
-    <BrowserRouter basename='/'>
+    <BrowserRouter basename='/treelink'>
       <Routes>
         <Route path = "/">
           <Route index element={<Homepage />} />
